7050b8258611beea09d4840b5ba39606c5643728
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-03-02 11:22:21 +0500

    routes/web.php fayliga o'zgartirish kiritildi

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     
     
+    
+    
     return view('welcome');
 });
 
 
 
-
+//bu sikl
 for ($i = 1; $i <= 100; $i++) {
    
-    $controller = "App\Http\Controllers\User{$i}Controller";
+    $controller = "App\Http\Controllers\User{$i}Controller";;
    
     Route::get("user{$i}", [$controller, 'index']);
 }
